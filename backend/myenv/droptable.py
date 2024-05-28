f7d5f9e2d5c7c52c53d76f107d17f313d5561e41
Kaleab Samuel <thekaleabsamuel@gmail.com>
2024-05-28 11:31:24 -0600

    Drop all tables from the database and delete all user data

--- backend/myenv/droptable.py
+++ backend/myenv/droptable.py
@@ -1,10 +1,11 @@
 # drop_table.py
 from database import db
+from models import User, Coach, Questionnaire, QuestionnaireData, UserAvailability, CoachAvailability, Appointment
 from app import app  # import your Flask application
 
-def drop_table(table_name):
-    with app.app_context():  # set up an application context
-        db.Table(table_name, db.metadata, autoload_with=db.engine).drop(db.engine)
+# def drop_table(table_name):
+with app.app_context():  # set up an application context
+    User.query.delete ()
+    db.session.commit()
 
-if __name__ == "__main__":
-    drop_table('platform_usage')
+
